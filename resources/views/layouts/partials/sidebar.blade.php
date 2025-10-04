64157ed1911d354a63f1ec4eb883668f63dff80a
komyor09 <komyor09@gmail.com>
2025-10-04 08:41:09 +0500

    Удаление элемента "Панель управления" из бокового меню и исправление ссылки на "Товары"

--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -2,11 +2,10 @@
 
     <!-- Add Sidebar Menu Items Here -->
 
-    <x-maz-sidebar-item name="Панель управления" :link="route('dashboard')" icon="bi bi-grid-fill"></x-maz-sidebar-item>
     <x-maz-sidebar-item name="Заказы" :link="route('orders')" icon="bi bi-cart3"></x-maz-sidebar-item>
     <x-maz-sidebar-item name="Доставшики" :link="route('deliver.index')" icon="bi bi-truck"></x-maz-sidebar-item>
     <x-maz-sidebar-item name="Клиенты" :link="route('client')" icon="bi bi-people"></x-maz-sidebar-item>
-    <x-maz-sidebar-item name="Товары" :link="route('products')" icon="bi bi-boxes"></x-maz-sidebar-item>
+    <x-maz-sidebar-item name="Товары" :link="route('product.index')" icon="bi bi-boxes"></x-maz-sidebar-item>
     <x-maz-sidebar-item name="Управление" icon="bi bi-gear">
         <x-maz-sidebar-sub-item name="Добавить курьера" :link="route('deliver.create')"
             icon="bi bi-person-plus"></x-maz-sidebar-sub-item>
